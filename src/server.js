fc87a8f9b8fdc735a856bb7e4ed36460474f0be2
shy060817 <alex.wanjun@gmail.com>
2019-11-06 22:44:44 +0900

    Add morgan logging middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
 const PORT = process.env.PORT || 4000;
 
 const typeDefs = `
@@ -17,6 +18,8 @@ const server = new GraphQLServer({
   typeDefs,
   resolvers
 });
+
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhost:${PORT}`)
